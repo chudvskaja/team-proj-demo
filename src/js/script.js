0cd772b6463d879c3182d720e120157ca30bf4c9
GutMaria <gootmaria@gmail.com>
2022-01-17 02:04:24 +0200

    add styles

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,6 @@ $(document).ready(function () {
     });
 });
 
-    $('.slider').slick({
-        arrows: false,
-        dots: true,
-        centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-    });
+
 
 
