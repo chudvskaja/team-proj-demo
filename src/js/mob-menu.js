8db6ffdabde2c2a3f74e743e8ea0f75821150e3e
Kseniia Biriukova <chudvskaja.ksenija@icloud.com>
2022-01-19 17:29:33 +0100

    Last fix of header and hero

---
+++ src/js/mob-menu.js
@@ -0,0 +1,31 @@
+
+   
+(() => {
+    const mobileMenu = document.querySelector('.header__list');
+    const openMenuBtn = document.querySelector('.menu-button-open');
+    const closeMenuBtn = document.querySelector('.menu-button-close');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-hidden');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
